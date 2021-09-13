2a38b6d6a4ae109b3a2e5f029fb0b9207449ed0b
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-13 02:36:43 -0300

    FEAT: Created update action

--- src/app/tasks/update/update-task.component.ts
+++ src/app/tasks/update/update-task.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { TaskService, Task } from '../shared';
+import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-task',
   templateUrl: './update-task.component.html',
-  styleUrls: ['./update-task.component.css']
+  styleUrls: ['./update-task.component.css'],
 })
 export class UpdateTaskComponent implements OnInit {
+  @ViewChild('taskForm') taskForm: NgForm;
+  task: Task;
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.task = this.taskService.findById(id);
   }
 
+  update(): void {
+    if (this.taskForm.form.valid) {
+      this.taskService.update(this.task);
+      this.router.navigate(['tasks']);
+    }
+  }
 }
