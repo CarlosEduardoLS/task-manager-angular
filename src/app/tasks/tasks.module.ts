e437347b2485020df762ae54daadddbd20dc6834
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-13 01:35:41 -0300

    FIX: Included new dependencies on tasks module

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,11 +1,13 @@
+import { RouterModule } from '@angular/router';
 import { TaskService } from './shared/task.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListTaskComponent } from './list';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ListTaskComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   providers: [TaskService],
 })
 export class TasksModule {}
