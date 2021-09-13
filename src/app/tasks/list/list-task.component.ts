920ea43f0b1c37b9ebb8e8a6ed730a5643f3baca
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-13 01:37:03 -0300

    FEAT: Created list function in list-task component

--- src/app/tasks/list/list-task.component.ts
+++ src/app/tasks/list/list-task.component.ts
@@ -1,15 +1,22 @@
+import { TaskService, Task } from '../shared';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-task',
   templateUrl: './list-task.component.html',
-  styleUrls: ['./list-task.component.css']
+  styleUrls: ['./list-task.component.css'],
 })
 export class ListTaskComponent implements OnInit {
+  tasks: Task[];
 
-  constructor() { }
+  constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
+    this.tasks = this.getTasks();
+    this.tasks = [new Task(1, 'Task 1', false), new Task(2, 'Task 2', true)];
   }
 
+  getTasks(): Task[] {
+    return this.taskService.getTasks();
+  }
 }
