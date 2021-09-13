2ded54d996a08634449522407dd0eefa76435ef1
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-13 00:03:07 -0300

    FIX: Included app routing module and tasks module in imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
+import { TasksModule } from './tasks/tasks.module';
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TasksModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
