c58265d6cfc6895184c54ab147db7f9e6e0589ad
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-13 01:34:26 -0300

    FEAT: Included in app routing module routes of tasks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { TaskRoutes } from './tasks/tasks-routing.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks/list',
+    pathMatch: 'full',
+  },
+  ...TaskRoutes,
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
