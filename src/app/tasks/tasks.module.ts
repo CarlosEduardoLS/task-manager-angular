e1357ef0f8726aa1deb7d5de35037f915196ac47
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-13 02:17:41 -0300

    FEAT: Included uptade task component in dependencies of task module

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -5,9 +5,10 @@ import { CommonModule } from '@angular/common';
 import { ListTaskComponent } from './list';
 import { FormsModule } from '@angular/forms';
 import { RegisterTaskComponent } from './register';
+import { UpdateTaskComponent } from './update';
 
 @NgModule({
-  declarations: [ListTaskComponent, RegisterTaskComponent],
+  declarations: [ListTaskComponent, RegisterTaskComponent, UpdateTaskComponent],
   imports: [CommonModule, RouterModule, FormsModule],
   providers: [TaskService],
 })
