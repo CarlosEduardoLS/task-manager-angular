4f5a828d8ab862a56d12c654a44649ec58dfea3b
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-13 02:19:10 -0300

    FEAT: Created register action

--- src/app/tasks/register/register-task.component.ts
+++ src/app/tasks/register/register-task.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { TaskService } from './../shared/task.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Task } from '../shared';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-task',
   templateUrl: './register-task.component.html',
-  styleUrls: ['./register-task.component.css']
+  styleUrls: ['./register-task.component.css'],
 })
 export class RegisterTaskComponent implements OnInit {
+  @ViewChild('taskForm') taskForm: NgForm;
+  task: Task;
 
-  constructor() { }
+  constructor(private taskService: TaskService, private router: Router) {}
 
   ngOnInit(): void {
+    this.task = new Task();
   }
 
+  register(): void {
+    if (this.taskForm.form.valid) {
+      this.taskService.create(this.task);
+      this.router.navigate(['/tasks']);
+    }
+  }
 }
